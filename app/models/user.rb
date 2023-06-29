3c5471a0e56dd099502c86a4b36c48a922a68633
Bateyjosue <josuebatey19@gmail.com>
2023-06-29 17:25:26 +0200

    Generate User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable
   belongs_to :role
   has_many :books
   validates :full_name, presence: true
